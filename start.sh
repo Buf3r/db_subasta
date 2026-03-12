de1936fcb68070f1acb3e5ba1e49a01b96ef0bfa
Carlos Daniel Morillo <xmorillocdx@gmail.com>
2026-03-12 01:20:52 -0400

    Fix bash variable names with dots 4

--- start.sh
+++ start.sh
@@ -9,4 +9,5 @@ database.default.username = ${database_default_username}
 database.default.password = ${database_default_password}
 database.default.port = ${database_default_port}
 database.default.DBDriver = MySQLi
+
 EOF
