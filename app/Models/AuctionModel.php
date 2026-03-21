6e4140287615c5256971ee54b218ae8207bab8da
Carlos Daniel Morillo <xmorillocdx@gmail.com>
2026-03-21 13:05:12 -0400

    Add category to auction select query

--- app/Models/AuctionModel.php
+++ app/Models/AuctionModel.php
@@ -49,7 +49,7 @@ class AuctionModel extends Model
 
    public function getAuction($id = NULL, $status = 'open', $where = NULL, $allStatus = false, $page = 1, $city = NULL)
 {
-    $select = 'auctions.auction_id, items.item_id, items.user_id, item_name, description, items.initial_price, items.location, items.condition, auctions.final_price, auctions.winner_user_id, auctions.status, auctions.date_completed, auctions.created_at';
+    $select = 'auctions.auction_id, items.item_id, items.user_id, item_name, description, items.initial_price, items.location, items.condition, items.category, auctions.final_price, auctions.winner_user_id, auctions.status, auctions.date_completed, auctions.created_at';
 
     if ($allStatus) {
         $whereArray = [
